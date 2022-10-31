aeee40e9c7931039edcd4d5b3b42b650cd4de630
sangmin <xectler@gmail.com>
2022-11-01 00:35:21 +0900

    update

--- practice.py
+++ practice.py
@@ -4,4 +4,33 @@ import urllib.request
 with open("index.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
+print(soup.title)
+
 print(soup.title.name)
+
+print(soup.title.string)
+
+print(soup.title.parent)
+
+print(soup.title.parent)
+
+soup_find = soup.find("div")
+
+print(soup_find)
+
+soup_find_all = soup.find_all("div")
+
+print(soup_find_all)
+
+soup.find('a').get('href')
+soup.find('a').get_text()
+
+site_names = soup.find_all('a') # 모든 a 태그
+
+for name in site_names:
+	print(name.get('href'))
+	print(name.get_text())
+
+id1 = soup.select('div#id1') # [id1]
+class1 = soup.select('div.class1') # [class1]
+class1_a = soup.select('div.class1 a')
