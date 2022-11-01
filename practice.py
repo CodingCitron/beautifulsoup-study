8d949214553bd56d9fea63a352c13f816971f3e2
sangmin <xectler@gmail.com>
2022-11-02 00:16:07 +0900

    update

--- practice.py
+++ practice.py
@@ -34,3 +34,17 @@ for name in site_names:
 id1 = soup.select('div#id1') # [id1]
 class1 = soup.select('div.class1') # [class1]
 class1_a = soup.select('div.class1 a')
+
+with open("contents.html", 'r', encoding="UTF-8") as fp:
+    soup2 = BeautifulSoup(fp, 'html.parser') 
+
+title  = soup2.find('p', { 'id': 'title' })
+contents = soup2.find_all('p', { 'id': 'content' })
+
+print(title)
+print(contents)
+
+for content in contents:
+    print(content.get_text())
+
+# 인터넷 웹페이지에서 가져오기
